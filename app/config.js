5c2fbea3c5fc49e128ef2fc359fc087cda842c6c
Shane Rogers <shane@repllabs.com>
2015-09-29 22:54:39 -0700

    Update port and env var references

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 /*eslint-env node */
-export const RAILSRoot = process.env.NODE_ENV === 'production' ? 'https://refugewe-server.herokuapp.com': 'http://127.0.0.1:3000';
-export const HAPIRoot = process.env.NODE_ENV === 'production' ? 'https://refugewe.herokuapp.com': 'http://127.0.0.1:3700';
+export const RAILSRoot = process.env.NODE_ENV === 'production' ? 'https://refugewe-server.herokuapp.com': 'http://127.0.0.1:7000';
+export const HAPIRoot = process.env.NODE_ENV === 'production' ? 'https://refugewe.herokuapp.com': 'http://127.0.0.1:3500';
 export const GA_KEY = process.env.GA_KEY || '';
